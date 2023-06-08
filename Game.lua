68679c4e930f2afbbd5a8b931d0bcd55edd67be8
NinjaAdam <116517776+NinjaAdam@users.noreply.github.com>
2023-06-07 22:18:26 -0500

    Update Game.lua

--- Game.lua
+++ Game.lua
@@ -8,10 +8,12 @@ local component = require("component")
 local x = 1
 local y = math.random(1,50)
 
-while x<100 do
-    --local _,_,mx,my = event.pull("touch")
+while x<130 or x == mx and y == my do
+    local _,_,mx,my = event.pull("touch")
     os.execute("clear")
     component.gpu.set(x, y, "X")
     
     x = x+0.25
 end
+
+os.execute("clear")
